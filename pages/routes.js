3665e452d27a17ffc05b20d588051630cf230f69
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-10 23:35:13 +0300

    initial commit

---
+++ pages/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { index } = require('./controllers');
+const { test } = require('./controllers');
+
+router.get('/', index);
+router.get('/test.html', test);
+
+module.exports = router;
