cfb42d3948838f20746455743d91959be0e221cd
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-10 23:46:35 +0300

    убрала тестовые данные

--- pages/controllers.js
+++ pages/controllers.js
@@ -11,26 +11,6 @@ module.exports.index = function (req, res) {
       <body class="app">
         <h1>Hello</h1>
 	    <div class="app__wrapper" id="app"></div>
-	    <a href="/test.html">tets</a>
-      </body>
-    </html>
-    `);
-};
-
-module.exports.test = function (req, res) {
-	res.send(`
-    <html>
-      <head>
-        <meta charset="UTF-8">
-	    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Яндекс: Переговорки</title>
-        <script src="/scripts/test.js"></script>
-      </head>
-      <body class="app">
-        <h1>Test</h1>
-	    <div class="app__wrapper" id="app"></div>
-	    
       </body>
     </html>
     `);
--- pages/routes.js
+++ pages/routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { index } = require('./controllers');
-const { test } = require('./controllers');
 
 router.get('/', index);
-router.get('/test.html', test);
 
 module.exports = router;
