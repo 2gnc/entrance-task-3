bed1188eee448aae11fff63dfe1dfebf5fc30be4
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-11 00:45:01 +0300

    проверила связь react и express

--- pages/routes.js
+++ pages/routes.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { index } = require('./controllers');
+//const { test } = require('./controllers');
 
-router.get('/', index);
+router.get( '/', index );
+router.get( '/test', function (req, res) {
+	res.send(
+  
+		[
+			{
+				id: 1,
+				username: "samsepi0l"
+			}, {
+				id: 2,
+				username: "D0loresH4ze"
+			},
+		]
+
+    );
+} );
+
 
 module.exports = router;
