41100a53f111a07ae00436b3641940f59632df71
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-11 23:45:28 +0300

    добавила к express использование cors()

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const bodyParser = require('body-parser');
 const pagesRoutes = require('./pages/routes');
 const graphqlRoutes = require('./graphql/routes');
 
+const cors = require('cors');
+
 const app = express();
+
+app.use( cors() );
 
 app.use(bodyParser.json());
 
 app.use('/', pagesRoutes);
 app.use('/graphql', graphqlRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(3001, () => console.log('Express app listening on localhost:3001'));
