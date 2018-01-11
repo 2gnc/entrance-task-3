700e42cfeeb07451b7d2b7b95e6c8410638904ac
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-11 16:51:22 +0300

    не получилось

--- pages/routes.js
+++ pages/routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { index } = require('./controllers');
-//const { test } = require('./controllers');
+
+//try1
+const {graphql} = require('graphql');
+const scheme = require('./../models/scheme');
+
+function query (str) {
+	return graphql(scheme, str);
+};
+
+
+//
+
 
 router.get( '/', index );
+
 router.get( '/test', function (req, res) {
 	res.send(
   
@@ -18,7 +30,18 @@ router.get( '/test', function (req, res) {
 		]
 
     );
-} );
+});
+
+router.get( '/users', function (req, res) {
+// как отправить запрос чтобы получить ответ от grqphql??
+// возвращается пустой запрос
+// вроде как нужна scheme, не пойму, где она
+
+	let x = query( 'query {users {id, login }}');
+
+	res.send( x );
+
+});
 
 
 module.exports = router;
