b95e23eb31d0d207a8d39588fe8c66fe0976cb5a
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-11 23:51:13 +0300

    добавила компонент

---
+++ peregovorki/src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+	render(){
+		return (
+			<header className="header">
+				<div className="wrapper header__layout">
+					<a className="logo__link" href="/">
+						<img className="logo" src="../img/logo.svg" alt"Яндекс переговорки"/>
+					</a>
+				</div>
+			</header>);
+		}
+	};
