7bdf10ff4c8fb506b06fc4f09b606ddbbd5553dc
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-11 23:44:39 +0300

    убрала ненужный код

--- pages/routes.js
+++ pages/routes.js
@@ -2,46 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { index } = require('./controllers');
 
-//try1
-const {graphql} = require('graphql');
-const scheme = require('./../models/scheme');
-
-function query (str) {
-	return graphql(scheme, str);
-};
-
-
-//
-
-
 router.get( '/', index );
-
-router.get( '/test', function (req, res) {
-	res.send(
-  
-		[
-			{
-				id: 1,
-				username: "samsepi0l"
-			}, {
-				id: 2,
-				username: "D0loresH4ze"
-			},
-		]
-
-    );
-});
-
-router.get( '/users', function (req, res) {
-// как отправить запрос чтобы получить ответ от grqphql??
-// возвращается пустой запрос
-// вроде как нужна scheme, не пойму, где она
-
-	let x = query( 'query {users {id, login }}');
-
-	res.send( x );
-
-});
 
 
 module.exports = router;
