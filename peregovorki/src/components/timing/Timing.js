4952aac41281b32aa64a9580bf414398511f9a1a
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-12 13:42:41 +0300

    новый компонент

---
+++ peregovorki/src/components/timing/Timing.js
@@ -0,0 +1,14 @@
+//обертка
+import React, { Component } from 'react';
+
+export default class Timing extends Component {
+	render() {
+		return(
+			<div className="timing">
+				<div className="wrapper timing__layout">
+					
+				</div>
+			</div>
+		)
+	}
+}
