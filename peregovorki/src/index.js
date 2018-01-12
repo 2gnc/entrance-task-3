fa03a5f24241feb3314c6166fd362df7639c268e
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-12 12:23:02 +0300

    убрала неиспользуемый код

--- peregovorki/src/index.js
+++ peregovorki/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 
 import { ApolloProvider } from 'react-apollo';
@@ -25,5 +23,3 @@ ReactDOM.render(
 	document.getElementById('root')
 );
 
-// что это такое?
-registerServiceWorker();
