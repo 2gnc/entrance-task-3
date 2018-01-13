d9147d9193c54dd7f756f0ebe5fa7099ff08507a
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-13 22:05:48 +0300

    добавила пропс выводить или нет кнопку

--- peregovorki/src/components/header/Header.js
+++ peregovorki/src/components/header/Header.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import Button from './../elements/Button';
+import {Link} from 'react-router'
 
 export default class Header extends Component {
+	
 	render(){
+		let button;
+		
+		if(this.props.hasButton === true) {
+			button = <Link to="/test"><Button modificator="btn--nomob" command="Создать встречу" /></Link>
+		} else {
+			button = '';
+		}
+		
 		return (
 			<header className="header">
 				<div className="wrapper header__layout">
 					<a className="logo__link" href="/">
 						<img className="logo" src="img/logo.svg" alt="Яндекс переговорки"/>
 					</a>
-					<Button modificator="btn--nomob" command="Создать встречу" />
+					{button}
+					
 				</div>
 			</header>);
 		}
