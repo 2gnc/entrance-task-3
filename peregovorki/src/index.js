a91257357c2fa0e3de25a5e8ee3af5cfa6f13e59
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-15 00:37:23 +0300

    изменила роутинг, подключила компонент

--- peregovorki/src/index.js
+++ peregovorki/src/index.js
@@ -6,13 +6,12 @@ import {Router, Route, IndexRoute, browserHistory } from 'react-router';
 import Main from './layouts/Main';
 import Test from './components/elements/Test';
 
-
-
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import gql from 'graphql-tag';
+import Eventeditor from "./components/eventeditor/Eventeditor";
 
 const client = new ApolloClient({
 	link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
@@ -25,7 +24,7 @@ ReactDOM.render(
 		<Router history={browserHistory}>
 			<Route path="/" component={Main} >
 				<IndexRoute component={App} />
-				<Route path="test" component={Test} />
+				<Route path="eventedit/:eventid" component={Eventeditor} />
 				<Route path="event" component={Test} >
 					<Route path=":eventId" component={Test} />
 				</Route>
