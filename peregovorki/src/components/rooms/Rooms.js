1d035e17c3bcd5cb96c0714f51439a72bbd78861
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-15 00:34:05 +0300

    добавила код смены модификаторов у комнат

--- peregovorki/src/components/rooms/Rooms.js
+++ peregovorki/src/components/rooms/Rooms.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
 import Floor from '../floor/Floor';
+import $ from 'jquery';
 
 export default class Rooms extends Component {
-
+	
+	componentDidMount() {
+		(function ($){
+			if ( $( '.schedule__aside' ).length > 0 ) {
+				const aside = $( '.schedule__aside' )[ 0 ].clientWidth;
+				const roomsbox = $( '.rooms' );
+				const rooms = $ ( '.room__name' );
+				const floors = $( '.rooms__floor-name' );
+				let check = false;
+				
+				document.addEventListener ( 'scroll', switcher );
+				
+				function switcher () {
+					
+					if ( window.pageXOffset > aside && !check ) {
+						check = true;
+						rooms.toggleClass ( 'room__name--swiped' );
+						roomsbox.toggleClass( 'rooms--swiped' );
+						floors.toggleClass( 'rooms__floor-name--swiped' );
+					}
+					
+					if ( window.pageXOffset <= aside && check ) {
+						check = false;
+						rooms.toggleClass ( 'room__name--swiped' );
+						roomsbox.toggleClass( 'rooms--swiped' );
+						floors.toggleClass( 'rooms__floor-name--swiped' );
+						roomsbox.removeAttr( 'style' );
+					}
+					
+					if( window.pageXOffset > aside ) {
+						roomsbox.css( 'left', window.pageXOffset );
+					}
+					
+				}
+			}
+		}($));
+	}
+	
 	render() {
 	
 	let floors = this.props.rooms.map(
@@ -11,8 +49,6 @@ export default class Rooms extends Component {
 			return <Floor number={i} key={i} rooms={rooms} />
 		}
 	);
-	
-	//console.log("Rooms все пропсы", this.props);
 	
 	return (
 		<div className = "rooms">
