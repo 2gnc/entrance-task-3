b11eb12fd38dc4e1c60edff406fbbcaa66c56357
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-16 17:21:48 +0300

    добавила условия рендеринга (в рекомендациях или в расписании)

--- peregovorki/src/components/room/Room.js
+++ peregovorki/src/components/room/Room.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 
 export default class Room extends Component {
+	constructor( props ) {
+		super( props );
+		this.state = {
+			isSelected: this.props.isSelected,
+		};
+	}
+
 	render() {
-		return (
-			<div className="room" data-roomid={this.props.datay}>
-				<div className="room__name">{this.props.name}</div>
-				<div className="room__desc">{"Вместимость: " + this.props.cap + " чел." } </div>
-			</div>
-		)
+		let modifier = '';
+		if( this.state.isSelected === true ) {
+			modifier = ' recomendation--selected';
+		}
+
+		if (this.props.layout === "infloors") {
+			return (
+				<div className="room" data-roomid={this.props.datay}>
+					<div className="room__name">{this.props.name}</div>
+					<div className="room__desc">{"Вместимость: " + this.props.cap + " чел." } </div>
+				</div>
+			)} else if (this.props.layout === "inrecomendations") {
+			return (
+					<div className={"recomendation" + modifier} data-roomid={this.props.roomId} >
+						<span className="recomendation__time">16:00 &ndash; 16:30</span>
+						<span className="recomendation__room">
+							{this.props.name} &middot; {this.props.number + " этаж" }
+							</span>
+					</div>
+				)
+		}
 	}
 }
