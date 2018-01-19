394916a0ea725a98db408b76bbf4140a4ec952fb
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-19 14:57:09 +0300

    заполняется сетка корректно

--- peregovorki/src/components/scheduleDay/ScheduleDay.js
+++ peregovorki/src/components/scheduleDay/ScheduleDay.js
@@ -12,6 +12,7 @@ export default class ScheduleDay extends Component {
 		this.getNodes = this.getNodes.bind(this);
 		this.pasteInners = this.pasteInners.bind(this);
 		this.makeDooms = this.makeDooms.bind(this);
+		this.fillByEmpty = this.fillByEmpty.bind(this);
 	}
 	
 	componentDidMount() {
@@ -27,6 +28,7 @@ export default class ScheduleDay extends Component {
 		//console.log( 'nodezz', this.getNodes(todayEvents) );
 		//console.log('nodesAndInners', nodesAndInners);
 		this.makeDooms(nodesAndInners);
+		this.fillByEmpty();
 		// создадим обычный массив объектов с нодами и массивом его иннеров
 	}
 	
@@ -40,6 +42,7 @@ export default class ScheduleDay extends Component {
 		//console.log( 'nodezz', this.getNodes(todayEvents) );
 		console.log('nodesAndInners', nodesAndInners);
 		this.makeDooms(nodesAndInners);
+		this.fillByEmpty();
 	}
 	getNodes( todayEvents ) {
 
@@ -168,26 +171,101 @@ export default class ScheduleDay extends Component {
 				let inners = arr[i].inners; // внутренние
 				let node = arr[i].node; // нода
 				
-				if (inners.length === 1) {
+				if (inners.length === 1) { // пересмотреть ширину эмпти и генерацию ширины бизи
 					if( inners[0].beginInterval === 1 &&  inners[0].width === 12) { //с начала и до конца на все интервалы
-						node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w12" data-event="' + inners[0].room + '"></div>' );
+						node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w12" data-event="' + inners[0].eventId + '"></div>' );
 					} else if ( inners[0].beginInterval === 1 &&  inners[0].width < 12 ) { // сначала и короткий, он является завершающим в любом случае, раз после него пустой блок
 						let emptywidth = 12 - inners[0].width;
 						if( inners[0].width > 0 ) {
-							node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].room + '"></div>' );
+							node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].eventId + '"></div>' );
 						}
 						node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + emptywidth +'"></div>' );
 					} else if ( inners[0].beginInterval !== 1 &&  inners[0].width < 12 ) {
 						let emptywidth = 12 - inners[0].width;
 						node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + emptywidth +'"></div>' );
 						if( inners[0].width > 0 ) {
-							node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].room + '"></div>' );
+							node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].eventId + '"></div>' );
 						}
 					}
+				} else if ( inners.length === 2 ) {
+					node.empty();
+					let e1w;
+					let e2w;
+					let e3w;
+					if ( inners[0].beginInterval === 1 ) { // если занято сначала
+						e1w = inners[1].beginInterval - inners[0].width - 1;
+						e2w = 12 - inners[1].width - e1w - inners[0].width - 1;
+						// первый полный, он всегда конечный
+						node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].eventId + '"></div>' );
+						// первый пустой
+						node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + e1w +'"></div>' );
+						// второй полный, он всегда конечный
+						node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + 'r" data-event="' + inners[1].eventId + '"></div>' );
+						// второй пустой
+						if (e2w) {node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + e2w +'"></div>' );}
+					} else { // если сначала пусто
+						e1w = inners[0].beginInterval - 1;
+						e2w = inners[1].beginInterval - inners[0].width - e1w - 1;
+						e3w = 12 - inners[1].width - e2w - inners[0].width - e1w;
+						// первый пустой
+						node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + e1w +'"></div>' );
+						// первый полный
+						node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].eventId + '"></div>' );
+						// второй пустой
+						node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + e2w +'"></div>' );
+						// второй полный (он конечный, если e3w === 0 и не конечный, если есть третий пустой интервал)
+						if (e3w > 0) {
+							if ( inners[1].ifEnding ) {
+								// второй полный конечный
+								node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + 'r" data-event="' + inners[1].eventId + '"></div>' );
+							} else {
+								// второй полный не конечный
+								node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + '" data-event="' + inners[1].eventId + '"></div>' );
+							}
+							// третий пустой
+							node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + e3w +'"></div>' );
+						} else {
+							if ( inners[1].ifEnding ) {
+								// второй полный конечный
+								node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + 'r" data-event="' + inners[1].eventId + '"></div>' );
+							} else {
+								// второй полный неконечный
+								node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + '" data-event="' + inners[1].eventId + '"></div>' );
+							}
+						}
+						// третий пустой если он не 0
+					}
+					
+					
+					// if ( inners[0].beginInterval === 1 ) {
+					// 	node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[0].width + 'r" data-event="' + inners[0].eventId + '"></div>' ); // первый занятый
+					// 	let emptywidth1 = 12 - inners[1].beginInterval - inners[0].width - 1;
+					// 	let emptywidth2 = 12 - inners[0].width - inners[1].width - emptywidth1;
+					// 	node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + emptywidth1 +'"></div>' ); // первый пустой
+					// 	let needR =	() => { (inners[1].ifEnding)? needR = 'r' : needR = '' };
+					// 	node.append( '<div class="schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + inners[1].width + needR() + ' data-event="' + inners[1].eventId + '"></div>' ); // втрой занятый
+					// 	node.append( '<div class="schedule__innerslot schedule__innerslot--emptyw' + emptywidth1 +'"></div>' ); // второй пустой
+					// }
 				}
 			}
 		}
 	}
+	fillByEmpty() {
+		let allrows = $('.schedule__rowslot');
+		for ( let i = 0; i < allrows.length; i++ ) {
+			if ( !allrows[i].classList.contains( 'schedule__rowslot--floor' ) && allrows[i].childNodes.length === 0 ) {
+				$(allrows[i]).append( '<div class="schedule__innerslot schedule__innerslot--emptyw12"></div>' );
+			}
+		}
+		//console.log( allrows[0].classList.contains( 'schedule__rowslot--floor' ) );
+		//for ( let i = 0; i < allrows.length; i++ ) {
+		// 	for (let row in allrows) {
+		// 		if ( !row.classList.contains('schedule__rowslot--floor') && row.childNodes.length === 0 ) {
+		// 			console.log ( row );
+		// 		}
+			// }
+		//}
+	};
 	// getDoomSlots() {
 	// 	let slots = document.getElementsByClassName( 'schedule__rowslot' );
 	// 	let targetSlots = [];
@@ -207,14 +285,6 @@ export default class ScheduleDay extends Component {
 				dateEnd: "2017-12-13T20:20:36.981Z",
 				users: [ {"id": "1"}, {"id": "2"} ],
 				room: {"id": "1"}
-			},
-			{
-				id: "2",
-				title: "Test",
-				dateStart: "2018-01-18T19:15:36.981Z",
-				dateEnd: "2018-01-18T19:40:36.981Z",
-				users: [ {"id": "1"}, {"id": "2"} ],
-				room: {"id": "2"}
 			},
 			{
 				id: "3",
@@ -297,6 +367,46 @@ export default class ScheduleDay extends Component {
 				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
 				room: {"id": "5"}
 			},
+			{
+				id: "12",
+				title: "Test2",
+				dateStart: "2018-01-19T11:10:00.981Z",
+				dateEnd: "2018-01-19T11:30:00.981Z",
+				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
+				room: {"id": "3"}
+			},
+			{
+				id: "13",
+				title: "Test2",
+				dateStart: "2018-01-19T11:40:00.981Z",
+				dateEnd: "2018-01-19T11:55:00.981Z",
+				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
+				room: {"id": "3"}
+			},
+			{
+				id: "14",
+				title: "Test2",
+				dateStart: "2018-01-19T15:00:00.981Z",
+				dateEnd: "2018-01-19T15:20:00.981Z",
+				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
+				room: {"id": "3"}
+			},
+			{
+				id: "15",
+				title: "Test2",
+				dateStart: "2018-01-19T15:30:00.981Z",
+				dateEnd: "2018-01-19T16:00:00.981Z",
+				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
+				room: {"id": "3"}
+			},
+			{
+				id: "16",
+				title: "Test2",
+				dateStart: "2018-01-19T15:00:00.981Z",
+				dateEnd: "2018-01-19T16:00:01.981Z",
+				users: [ {"id": "1"}, {"id": "2"}, {"id": "4"} ],
+				room: {"id": "1"}
+			},
 			
 		];
 		let x = testEvent.map(( item, i ) => {
@@ -352,7 +462,11 @@ export default class ScheduleDay extends Component {
 					 */
 					let getBeginInterval = () => {
 						if ( +begin.format ( 'HH' ) === itm ) {
-							return Math.floor ( begin.format ( 'mm' ) / 5 );
+							if ( Math.ceil ( begin.format ( 'mm' ) / 5 ) === 0 ) {
+								return 1;
+							} else {
+								return Math.ceil ( begin.format ( 'mm' ) / 5 +1 );
+							}
 						} else {
 							return 1;
 						}
@@ -361,28 +475,40 @@ export default class ScheduleDay extends Component {
 					 * Function getIntervalWidth возвращает ширину иннер слота в интервалах (ширина может быть от 1 до 12 включительно), в зависимости от позиции слота-родителя (является ли он серединой, началом или концом события)
 					 * @return {number}
 					 */
-					let getIntervalWidth = () => {
-						if ( +begin.format ( 'HH' ) === itm && slots.length === 1 ) {
-							return (
-								( Math.floor ( end.format ( 'mm' ) / 5 ) + 1 ) - getBeginInterval ()
-							)
-						} else if ( +begin.format ( 'HH' ) === itm && slots.length > 1 ) {
-							return 12 - getBeginInterval () +1;//!!
-						} else if ( dur > 60 && +begin.format ( 'HH' ) < itm && +end.format ( 'HH' ) > itm ) {
+					let getIntervalWidth = ( startInterval ) => {
+						let endInterval = Math.round( end.format ( 'mm' ) / 5 );
+						if ( dur > 60 && +begin.format ( 'HH' ) < itm && +end.format ( 'HH' ) > itm ) { // слотов много, это средний слот, он полностью заполнен
+							return 12
+						} else if (dur === 60 && +begin.format ( 'HH' ) === itm && +end.format ( 'HH' ) > itm) {
+							return 12
+						} else if ( +begin.format ( 'HH' ) === itm && slots.length > 1) { // слотов много, это первый слот
+							if ( startInterval === 1 ) {
+								return 12 - startInterval + 1;
+							} else {
+								return 12 - startInterval +1;
+							}
+						} else if ( +begin.format ( 'HH' ) === itm && slots.length === 1  ) { // слот один
+							if ( startInterval === 1 ) {
+								return endInterval - startInterval +1;
+							} else {
+								return endInterval - startInterval +1;
+							}
+						} else if ( dur === 60 && +begin.format ( 'HH' ) === itm ) {
+							console.log('сработало');
 							return 12;
-						} else {
-							return Math.floor ( end.format ( 'mm' ) / 5 );
+						} else { // это последний слот
+							return endInterval;
 						}
 					};
 					
 					let getCss = () => {
 						let modifier = '';
-						if ( getIntervalWidth () === 12 ) {
+						if ( getIntervalWidth ( getBeginInterval() ) === 12 ) {
 							modifier = 12;
 						} else if ( getEnding () === true ) {
-							modifier = getIntervalWidth () + 'r';
+							modifier = getIntervalWidth ( getBeginInterval() ) + 'r';
 						} else {
-							modifier = getIntervalWidth ();
+							modifier = getIntervalWidth ( getBeginInterval() );
 						}
 						return 'schedule__innerslot schedule__innerslot--busy schedule__innerslot--w' + modifier;
 					};
@@ -392,7 +518,7 @@ export default class ScheduleDay extends Component {
 					};
 					/**
 					 * @typedef {object} Описывает один внутренний занятый слот в рамках одного таймслота (часа) для события. Если событие длится более часа, то задействовано будет больше одного таймслота.
-					 * @property {string} room идентификатор комнаты
+					 * @property {string} eventId идентификатор события
 					 * @property {number} timeslot Таймслот, внутри которого будет создан иннерслот
 					 * @property {number} beginInterval Интервал, с которого начинается иннер.
 					 * @property {number} width Количество занятых интервалов. Интервал - 5 минут. Если эвент занимает весь таймслот, то ширина иннера - 12.
@@ -400,10 +526,10 @@ export default class ScheduleDay extends Component {
 					 * @property {string} cssClass Строка с классом, DOM-элемента, соответствующего иннеру.
 					 */
 					return ({
-						room: eventId,
+						eventId: eventId,
 						timeslot: itm,
 						beginInterval: getBeginInterval (),
-						width: getIntervalWidth (),
+						width: getIntervalWidth ( getBeginInterval() ),
 						ifEnding: getEnding (),
 						cssClass: getCss (),  // тут модификатор, описывающий внутренний busy-слот, его ширину w1-w12, интервал, с которого он начинается (0-11), и в рамках данного timeslot оканчивается ли event или распространяется на след слот (если таймслотов >1, то распространяется во всех слотах кроме последнего), а также сгенерированную строку - класс для div-а
 					});
@@ -435,6 +561,8 @@ export default class ScheduleDay extends Component {
 			})
 			
 		});
+		console.log( 'x', x );
+		
 		return x;
 	};
 	
