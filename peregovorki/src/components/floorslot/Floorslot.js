ec7df3daaa52e3d229006acfe473f565d93ea7fe
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-20 11:53:15 +0300

    убрала консоль лог

--- peregovorki/src/components/floorslot/Floorslot.js
+++ peregovorki/src/components/floorslot/Floorslot.js
@@ -3,7 +3,6 @@ import Rowslot from '../rowslot/Rowslot'
 
 export default class Floorslot extends Component {
 	render () {
-		//console.log( "floorslot", this.props);
 		return (
 			<div className="schedule__floorslot">
 				<Rowslot addname="schedule__rowslot--floor"/>
