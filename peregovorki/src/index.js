3c0735d6da17538a9fd66b568d3de3fa1e4b7aad
Polyakova Kseniya <ksenia.v.polyakova@mail.ru>
2018-01-22 22:19:57 +0300

    почистила код

--- peregovorki/src/index.js
+++ peregovorki/src/index.js
@@ -9,7 +9,6 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import Eventeditor from './components/eventeditor/Eventeditor';
 import EventeditorWrapper from './components/eventeditorWrapper/EventeditorWrapper';
 
 const client = new ApolloClient({
