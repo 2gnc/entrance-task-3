05d1cd5ad2a5df6a1ab6901a54cf07c01e2f00cc
Ksenia Polyakova <ksenia.v.polyakova@mail.ru>
2018-01-23 13:18:39 +0300

    мелкие правки

--- peregovorki/src/components/header/Header.js
+++ peregovorki/src/components/header/Header.js
@@ -5,19 +5,20 @@ import {Link} from 'react-router';
 export default class Header extends Component {
 	
 	render(){
+		
 		let button;
 		
 		if(this.props.hasButton === true) {
-			button = <Link to="/eventedit/new"><Button modificator="btn--nomob" command="Создать встречу" /></Link>
+			button = <Link to = '/eventedit/new'><Button modificator = 'btn--nomob' command = 'Создать встречу' /></Link>
 		} else {
 			button = '';
 		}
 		
 		return (
-			<header className="header">
-				<div className="wrapper header__layout">
-					<a className="logo__link" href="/">
-						<img className="logo" src="http://localhost:3000/img/logo.svg" alt="Яндекс переговорки"/>
+			<header className = 'header'>
+				<div className = 'wrapper header__layout'>
+					<a className = 'logo__link' href='/'>
+						<img className = 'logo' src='http://localhost:3000/img/logo.svg' alt = 'Яндекс переговорки'/>
 					</a>
 					{button}
 					
